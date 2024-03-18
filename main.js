89ef11f1fda0aedaaabd78867ec35e178aed1ff2
Shirofcs <106327694+Shirofcs@users.noreply.github.com>
2024-03-18 07:38:54 -0300

    t4

---
+++ main.js
@@ -0,0 +1,12 @@
+const botoes = document.querySelectorAll(".botao");
+const textos=document.querySelectorAll(".aba-conteudo")
+for (let i =0 <botoes.length; i++){
+    botoes[i].onliclick=function () {
+        for (let j=0; j < botoes.length; j++)
+          botoes[j].classList.remove("ativo")
+          textosj]classList.remove("ativo")
+ }
+
+    botoes[i].classList.add("ativo")
+    textos[i].classList.add("ativo")
+}
